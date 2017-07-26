e81ea5932effcf7d1d6f02d150ac50658c76f856
mayak19-meet <mayak19@meet.mit.edu>
2017-07-26 18:23:16 +0300

    Stopping here

---
+++ tickets.py
@@ -0,0 +1,16 @@
+speed = int(input('what is your speed?'))
+is_birthday = bool(input('is it your birthday?'))
+if is birthday
+    if speed <31+5 :
+        print​(​'no ticket'​)
+    elif​ speed>30 and speed <51+5:
+        print​(​'small ticket'​)
+    else​:
+        print​(​'big ticket'​)
+else
+    if speed < 31:
+        print('no ticket')
+    elif spped> 30 and speed <51
+        print('small ticket')
+    else :
+        print('big ticket')
